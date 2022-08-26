711e3b77d5c2b395eaa9e0109fb0994c6db46b8a
arunkumar1322 <arunkumar.1322@gmail.com>
2022-08-26 14:43:54 +0800

    My first github commit

---
+++ MyFirstGitHubProject/Shared/MyFirstGitHubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstGitHubProjectApp.swift
+//  Shared
+//
+//  Created by Arun Development on 26/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstGitHubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
